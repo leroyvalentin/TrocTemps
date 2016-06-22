64bbfeb83a3f2752c63312bb1d943f4c17155c5b
valentinleroy <valentin.leroy.11@hotmail.fr>
2016-06-22 14:42:01 +0200

    scripts de gestion de base

---
+++ creation_de_la_base.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+   <head>
+    <?php include('include_head.php') ?>
+ </head>
+   
+   <body>
+      <ul class="nav">
+    <li class="nav-item">
+        <a href="index.php"> <span class="glyphicon glyphicon-home"></span></a>
+    </li>
+</ul>
+      <?php
+      include ('inclusion.php');
+ $l_base=tt_connectionbase();
+ 
+$requetes="";
+ 
+$sql=file("troctemps script database.sql"); // on charge le fichier SQL
+foreach($sql as $l){ // on le lit
+  if (substr(trim($l),0,2)!="--"){ // suppression des commentaires
+    $requetes .= $l;
+  }
+}
+ 
+$reqs = split(";",$requetes);// on sépare les requêtes
+foreach($reqs as $req){ // et on les éxécute
+  if (!mysql_query($req,$l_base) && trim($req)!=""){
+    die("ERROR : ".$req); // stop si erreur 
+  }
+}
+echo "base a été créer , si elle exister déjà elle a été restaurée";
+?>
+
+ </body>
+   
+</html>
---
+++ troctemps_script_database.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Mer 15 Juin 2016 à 11:36
+-- Version du serveur: 5.5.49-0ubuntu0.14.04.1
+-- Version de PHP: 5.5.9-1ubuntu4.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `TrocTemps`
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscrit`
+--
+
+DROP TABLE IF EXISTS `inscrit`;
+CREATE TABLE IF NOT EXISTS `inscrit` (
+  `num_inscrit` int(100) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `profession` varchar(30) NOT NULL,
+  `sexe` text NOT NULL,
+  `nom_ville` text NOT NULL,
+  `cp` text NOT NULL,
+  `rue` text NOT NULL,
+  `add_mail` text NOT NULL,
+  `telephone` text NOT NULL,
+  `description` text NOT NULL,
+  `pass` text NOT NULL,
+  PRIMARY KEY (`num_inscrit`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=REDUNDANT AUTO_INCREMENT=00 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
